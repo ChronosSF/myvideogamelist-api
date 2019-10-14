55e31b98cd611669468052cca5ce0a81ca83fb25
Stamen Stoychev <sstoychev@infragistics.com>
2019-10-14 21:10:15 +0300

    Migrating to .net core 3.0

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Mvgl
 {
@@ -40,16 +41,13 @@ namespace Mvgl
 					});
 			});
 
-
-			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+			
+			services.AddMvc(o => o.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		public void Configure(IApplicationBuilder app, IHostEnvironment env)
 		{
-
-		
-
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
